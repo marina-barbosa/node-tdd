2c426c112ed60f650dd587517a885b0636b3c09e
marina-barbosa <marinabarbosa.exp@gmail.com>
2024-12-06 00:13:44 -0300

    deve listar todos os users

--- src/app.js
+++ src/app.js
@@ -4,4 +4,17 @@ app.get('/', (_, res) => {
 	res.status(200).send('Hello World!');
 });
 
+app.get('/users', (_, res) => {
+  res.status(200).json([
+    {
+      name: 'Isaac Doe',
+      email: 'isaaclovehortencia@gmail.com',
+    },
+    {
+      name: 'Hortência Flores',
+      email: 'hortencia@gmail.com',
+    },
+  ]);
+});
+
 module.exports = app;
